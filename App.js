3d6ae9f7bcb792e644aa48e92f01f9366cd2c6f9
Calum <calum.rogers@ntlworld.com>
2021-11-04 20:34:25 +0000

    second commit

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
-import React,{useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState("Open up App.js to start working on your app!")
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change Text" onPress={() => setOutputText('The text changed!')}/>
+    <View>
+      <View>
+        <TextInput />
+        <Button title="ADD"/>
+      </View>
+      <View>
+
+      </View>
+      
       
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ 
 });
